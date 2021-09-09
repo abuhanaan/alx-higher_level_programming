d1e8c916b2fec42c2cc880f917191cc84fac1453
abuhanaan <adewunmi99@gmail.com>
2021-09-09 18:15:56 +0100

    Fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,4 +9,4 @@ b = 5
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)), end='')
+print("{} / {} = {}".format(a, b, div(a, b)))
