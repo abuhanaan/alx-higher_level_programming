4fac867b0b5b258f9f56adc96cd348b0d14a7cdc
abuhanaan <adewunmi99@gmail.com>
2021-09-09 10:00:27 +0100

    Fixed

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from add_0 import add
+
+def myfunction():
+    c = add(1, 2)
+    print("1 + 2 = {}".format(c), end='')
+
+myfunction()
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 1
+    b = 2
+    return a + b
 
+if __name__ == "__main__":
+    import sys
+    add(int(sys.argv[1]))
