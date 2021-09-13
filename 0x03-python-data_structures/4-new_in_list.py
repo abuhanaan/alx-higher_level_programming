ae1360ccc57048da7b8bce6ba79fcaf8f865c809
abuhanaan <adewunmi99@gmail.com>
2021-09-13 21:49:17 +0100

    replaces an element in a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,6 +6,7 @@ def new_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        copy = my_list
+        copy[idx] = element
+        return copy
 
