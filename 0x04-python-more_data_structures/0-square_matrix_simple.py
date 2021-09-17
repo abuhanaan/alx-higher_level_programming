22214434cbf2ea2c38f16d2ea763ac676c5c1292
abuhanaan <adewunmi99@gmail.com>
2021-09-17 20:57:07 +0100

    computes the square value of all integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 
-def search_replace(my_list, search, replace):
-    """
-    A function that replaces all occurrences
-    of an element by another in a new list
-    """
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-    return new_list
+def square_matrix_simple(matrix=[]):
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
