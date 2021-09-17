a00d79a0035b983b173f528d518a48e780fe530c
abuhanaan <adewunmi99@gmail.com>
2021-09-17 21:00:16 +0100

    replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,10 +6,8 @@ def uniq_add(my_list=[]):
     A function that adds all unique
     integers in a list (only once for each integer)
     """
-    new_list = []
-    sum = 0
-    for num in my_list:
-        if num not in new_list:
-            sum += num
-            new_list.append(num)
-    return sum
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
