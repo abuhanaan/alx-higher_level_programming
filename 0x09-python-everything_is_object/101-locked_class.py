4ab1bed535c6b12913f39666538d6a5c797ced65
abuhanaan <adewunmi99@gmail.com>
2021-10-06 12:02:05 +0100

    a class with conditions

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module defines a locked class"""
+
+
+class LockedClass:
+    """
+    A locked class that only lets the user dynamically 
+    create the instance attribute 'first_name'
+    """
+    __slots__=['first_name']
