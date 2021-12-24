22c29606c9ee3eede86c0cc4ee0887fec6d66fff
abuhanaan <adewunmi99@gmail.com>
2021-12-24 19:48:53 +0100

    Fixed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,13 +5,25 @@ in the states table of hbtn_0e_0_usa where name matches
 the argument. But this time, write one that is safe from
 MySQL injections!
 """
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                            passwd=sys.argv[2], db=sys.argv[3],
-                            port=3306)
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
-    [print(state) for state in cursor.fetchall() if state[1] == sys.argv[4]]
+    cursor.execute("""SELECT * FROM states
+        WHERE CAST(name AS BINARY) = CAST('{}' AS BINARY)
+        ORDER BY states.id;""".format(sys.argv[4]))
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    db.close()
+    
