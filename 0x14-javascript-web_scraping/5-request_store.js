fb4b571705d8b1695ab9131e62520d05adccde68
abuhanaan <adewunmi99@gmail.com>
2022-02-02 02:20:09 +0100

    gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,10 +5,4 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
-});
+request(url).pipe(fs.createWriteStream(filePath));
